4acd1e16bb8a7fea71539f7aaea93a96f1dafae6
Vlad_IV <ivanovicivasilevlad@gmail.com>
2019-03-27 00:26:13 +0200

    HomeworkWeek2

---
+++ app/src/main/java/com/example/javaone/FirstProblem.java
@@ -0,0 +1,15 @@
+package com.example.javaone;
+
+public class FirstProblem {
+
+    public static void main (String[] args)
+    {
+        int a = 6;
+        int b = 10;
+        double c = 0.2;
+        double x;
+
+        x = (a * b)/c;
+        System.out.println (x);
+    }
+}
---
+++ app/src/main/java/com/example/javaone/SeventhProblem.java
@@ -0,0 +1,29 @@
+package com.example.javaone;
+
+public class SeventhProblem
+{
+    public static void main (String[] args)
+    {
+        int thisArray[] = {1, 1, 3, 2, 2, 1};
+        int clumpsCount = 0;
+        int previousElement = thisArray[0] + 1;
+        boolean isOnClump = false;
+
+        for(int i:thisArray)
+        {
+            if(i == previousElement)
+            {
+                if(isOnClump == false)
+                {
+                    System.out.println(i + " " + previousElement);
+                    clumpsCount++;
+                    isOnClump = true;
+                }
+            }
+            else
+                isOnClump = false;
+            previousElement = i;
+        }
+        System.out.println("Number of clumps found: " + clumpsCount);
+    }
+}
